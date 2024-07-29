d581bf4135c5cea382811daf3fc0d636809bd9cf
zizon <zzdtsv@gmail.com>
2024-07-29 09:36:46 +0000

    correct annotation for testing config usage
    Signed-off-by: zizon <zzdtsv@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -857,10 +857,10 @@ CONF_Int64(object_storage_rename_file_request_timeout_ms, "30000");
 CONF_Strings(fallback_to_hadoop_fs_list, "");
 CONF_Strings(s3_compatible_fs_list, "s3n://, s3a://, s3://, oss://, cos://, cosn://, obs://, ks3://, tos://");
 CONF_mBool(s3_use_list_objects_v1, "false");
-CONF_String(jindo_fs_test_endpoint, "");
-// for jindo test only
 CONF_Strings(jindo_fs_list, "");
 CONF_String(jindo_user, "starrocks");
+// for jindo test only
+CONF_String(jindo_fs_test_endpoint, "");
 
 // Lake
 CONF_mBool(io_coalesce_lake_read_enable, "false");
